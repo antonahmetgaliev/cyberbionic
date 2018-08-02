121c9ba0e178fb9a5f5cfd446266e3bafca02732
Anton Ahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-02 11:12:13 +0300

    add ignore file

---
+++ Starter/HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi, e;
+            pi = 3.141592653d;
+            e = 2.7182818284590452d;
+            Console.WriteLine(pi);
+            Console.WriteLine(e);
+            Console.ReadKey();
+        }
+    }
+}
