207ed111eca804254df47191e8bf634068832239
Anton Ahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-02 11:31:43 +0300

    add ignore file

---
+++ Starter/Lesson2HomeWork2Task3/Lesson2HomeWork2Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson2HomeWork2Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a, b, c;
+            a = "\nмоя строка 1";
+            b = "\tмоя строка 2";
+            c = "\aмоя строка 3";
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
