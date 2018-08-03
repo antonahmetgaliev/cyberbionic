4072a47d504f49e3796872ecc561b8fbb29f1b77
Anton Ahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-03 13:28:06 +0300

    used do..while

--- Starter/L4HWT0_Weather/L4HWT0_Weather/Program.cs
+++ Starter/L4HWT0_Weather/L4HWT0_Weather/Program.cs
@@ -5,64 +5,64 @@ namespace L4HWT0_Weather
     class Program
     {
         static void Main(string[] args)
-        {            
-            bool exx = false;
-            while (exx != true)
+        {
+            do
             {
                 Console.WriteLine("Какая погода сегодня?(Введите '?' для подсказки)");
-                string WeatherToday = Console.ReadLine();                
+                string WeatherToday = Console.ReadLine();
                 switch (WeatherToday)
                 {
                     case "?":
-                        Console.WriteLine("(Хорошая,Плохая,Нормальная,Солнечная,Облачная,Дождь,Ливень,Гроза,Снегопад,Град");
+                        Console.WriteLine("Хорошая,Плохая,Нормальная,Солнечная,Облачная,Дождь,Ливень,Гроза,Снегопад,Град");
                         Console.WriteLine();
                         break;
                     case "Хорошая":
                         Console.WriteLine("{0} =  Good.", WeatherToday);
-                        if (Ques()) {exx = true; break;} else break;
+                        break;
                     case "Плохая":
                         Console.WriteLine("{0} =  Bad.", WeatherToday);
-                        if (Ques()) {exx = true; break;} else break;
+                        break;
                     case "Нормальная":
                         Console.WriteLine("{0} =  Normal.", WeatherToday);
-                        if (Ques()) {exx = true; break;} else break;
+                        break;
                     case "Солнечная":
                         Console.WriteLine("{0} =  Solar.", WeatherToday);
-                        if (Ques()) {exx = true; break;} else break;
+                        break;
                     case "Облачная":
                         Console.WriteLine("{0} =  Cloudy.", WeatherToday);
-                        if (Ques()) {exx = true; break;} else break;
+                        break;
                     case "Дождь":
                         Console.WriteLine("{0} =  Rain.", WeatherToday);
-                        if (Ques()) {exx = true; break;} else break;
+                        break;
                     case "Ливень":
                         Console.WriteLine("{0} =  Downpour.", WeatherToday);
-                        if (Ques()) {exx = true; break;} else break;
+                        break;
                     case "Гроза":
                         Console.WriteLine("{0} =  Storm.", WeatherToday);
-                        if (Ques()) {exx = true; break;} else break;
+                        break;
                     case "Снегопад":
                         Console.WriteLine("{0} =  Snowfall.", WeatherToday);
-                        if (Ques()) {exx = true; break;} else break;
+                        break;
                     case "Град":
                         Console.WriteLine("{0} =  Hailstones.", WeatherToday);
-                        if (Ques()) {exx = true; break;} else break;
+                        break;
                     default:
                         Console.WriteLine("Не правильныый ввод, пожалуйста, повторите попытку");
                         Console.WriteLine();
                         break;
                 }
             }
+            while (Ques());
         
         bool Ques()
         {
             Console.Write("Желаете продолжить?(Да/Нет) ");
             string choise = Console.ReadLine();
-            if (choise == "Нет") return true;
+            if (choise == "Нет") return false;
             else
             {
                 Console.WriteLine();
-                return false;
+                return true;
             }
         }
     }
