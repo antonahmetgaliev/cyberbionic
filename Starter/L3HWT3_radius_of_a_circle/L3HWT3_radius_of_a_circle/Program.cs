b3bb2365b00c6a837e2b7f77616f15c36b2497a7
Anton Ahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-03 10:38:36 +0300

    used Math.PI

--- Starter/L3HWT3_radius_of_a_circle/L3HWT3_radius_of_a_circle/Program.cs
+++ Starter/L3HWT3_radius_of_a_circle/L3HWT3_radius_of_a_circle/Program.cs
@@ -5,10 +5,9 @@ namespace L3HWT3_radius_of_a_circle
     class Program
     {
         static void Main(string[] args)
-        {
-            const double pi = 3.1415926535f;
+        {               
             byte r = 4;
-            var rez = pi * Math.Pow(r, 2);
+            var rez = Math.PI * Math.Pow(r, 2);
             Console.WriteLine("Area of a circle with radius {1} = {0} square meters", rez, r);
             Console.ReadKey();
         }
