270c458bb24f15053bf34fb56a5f4d3a2201a044
Anton Ahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-03 14:45:44 +0300

    add programm calculator

--- Starter/L4HWT3_interval/L4HWT3_interval/Program.cs
+++ Starter/L4HWT3_interval/L4HWT3_interval/Program.cs
@@ -6,7 +6,21 @@ namespace L4HWT3_interval
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool cycle = true;
+            do
+            {                
+                Console.Write("Input number - ");
+                string numberInStrnig = Console.ReadLine();
+                int number = Convert.ToInt32(numberInStrnig);
+                string interval = number >= 0 ? number >= 15 ? number >= 36 ? number >= 50 ? number >= 101 ? "Error! Input Other value." : "[50-100]" : "[36-50]" : "[15-35]" : "[0-14]" : "Error! Input other value.";
+                if (interval!= "Error! Input Other value.")
+                    Console.WriteLine("Number {0} is in the interval {1}\n", number, interval);
+                else Console.WriteLine(interval);
+                Console.Write("Would you like to continue?(y/n)");
+                string choise = Console.ReadLine();
+                if (choise == "y") cycle = true; else cycle = false;
+            }
+            while (cycle == true);
         }
     }
 }
