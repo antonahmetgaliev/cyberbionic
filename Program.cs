c8b8476166b1c6c933ef38f21350435bf52cd622
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-06 18:07:43 +0300

    new windows check

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MyFirsyConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello   World!");
             Console.ReadKey();
         }
     }
