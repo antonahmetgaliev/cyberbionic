1f72b271daf9b2e9fee90c06c0f7dae22e811cf7
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-06 18:14:03 +0300

    delete using

--- Starter/L5HWT1_graduation/L5HWT1_graduation/Program.cs
+++ Starter/L5HWT1_graduation/L5HWT1_graduation/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace L5HWT1_graduation
+﻿namespace L5HWT1_graduation
 {
     class Program
     {
