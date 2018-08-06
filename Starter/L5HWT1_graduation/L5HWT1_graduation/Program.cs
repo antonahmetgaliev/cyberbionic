0ddfba5217614fed6bff62f4f3db6219f6eeffbd
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-06 18:11:07 +0300

    new project graduation

---
+++ Starter/L5HWT1_graduation/L5HWT1_graduation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L5HWT1_graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
