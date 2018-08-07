b07918bfdd596973aa2c90ba68770ba7ad866042
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-07 13:08:05 +0300

    completed work on the program L5HWT1_graduation

--- Starter/L5HWT1_graduation/L5HWT1_graduation/Program.cs
+++ Starter/L5HWT1_graduation/L5HWT1_graduation/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace L5HWT1_graduation
+﻿using System;
+
+namespace L5HWT1_graduation
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int grad;
+            Console.Write("Input - ");
+            string strtoint = Console.ReadLine();
+            grad = Convert.ToInt32(strtoint);
+            bool grduation_check = grad != 0 && (grad & (grad - 1)) == 0 ? true : false;
+            Console.WriteLine(grduation_check);
+            Console.ReadKey();
         }
     }
 }
--- Starter/L5HWT1_graduation/L5HWT1_graduation/Readme.txt
+++ Starter/L5HWT1_graduation/L5HWT1_graduation/Readme.txt
@@ -2,3 +2,10 @@
 Известно, что у чисел, которые являются степенью двойки, только один бит имеет значение 1. 
 Используя Visual Studio, создайте проект по шаблону ConsoleApplication.  
 Напишите программу, которая будет выполнять проверку – является ли указанное число степенью двойки или нет. 
+
++Ask for a number
++Read the specified number
++Convert int to str
++Check whether the number is a power of two
++display the answer
++done
