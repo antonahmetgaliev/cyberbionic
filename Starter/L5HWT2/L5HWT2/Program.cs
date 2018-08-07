d4f0a61d91eb6181c4c54adf158b6d5f14cb369c
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-08 00:15:14 +0300

    add programm check even

---
+++ Starter/L5HWT2/L5HWT2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L5HWT2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number - ");
+            string numbstr = Console.ReadLine();
+            int numb = Convert.ToInt32(numbstr);            
+            bool check = ((numb&1)== 0); 
+            if (check)
+                Console.WriteLine("Number {0} even", numb);
+            else
+                Console.WriteLine("Number {0} odd", numb);
+            Console.ReadKey();
+        }
+    }
+}
