e0311fb2f5350e90c743cb5e982cf1e01f197622
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-08 02:10:30 +0300

    add square programm

---
+++ Starter/L6HWExtra1_square/L6HWExtra1_square/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace L6HWExtra1_square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input width - ");
+            string wtstr = Console.ReadLine();
+            int wt = Convert.ToInt32(wtstr);
+            Console.Write("Input height - ");
+            string hgstr = Console.ReadLine();
+            int hg = Convert.ToInt32(hgstr);
+            for (int h = 0; h<hg; h++)
+            {
+                for (int w = 0; w<wt; w++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
