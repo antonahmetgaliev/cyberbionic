1a1016c0f33ef4d98e18ed282aeef46094b15d19
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-08 01:18:35 +0300

    add Prize programm

---
+++ Starter/L5HWT4Prize/L5HWT4Prize/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace L5HWT4Prize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+                { 
+                    Console.Write("Input the number of years - ");
+                    string yearstr = Console.ReadLine();
+                    int year = Convert.ToInt32(yearstr);
+                    Console.Write("Input your salary - ");
+                    string prizestr = Console.ReadLine();
+                    int prize = Convert.ToInt32(prizestr);
+                    double prized=prize;
+                    string pr = "";
+                    if (year < 5)
+                    { prized *= prize * 0.1; pr = "10%"; }
+                    else
+                        if (year >= 5)
+                            if (year >= 10)
+                                if (year >= 15)
+                                    if (year >= 20)
+                                        if (year >= 25)
+                                            { prized *= 0.5; pr = "50%"; }
+                                        else
+                                            { prized *= 0.45; pr = "45%"; }
+                                    else
+                                        { prized *= 0.35; pr = "35%"; }
+                                else
+                                    { prized *= 0.25; pr = "25%"; }
+                            else
+                                { prized *= 0.15; pr = "15%"; }
+                    Console.WriteLine("With such length of service, the premium is {0} = {1}", pr, prized);
+                }
+            while (Ques()) ;            
+
+            bool Ques()
+            {
+                Console.Write("Would you like to continue?(Y/N) ");
+                string choise = Console.ReadLine();
+                if (choise == "N") return false;
+                else
+                {
+                    Console.WriteLine();
+                    return true;
+                }
+            }
+        }
+    }
+}
---
+++ Starter/L5HWT4Prize/L5HWT4Prize/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("L5HWT4Prize")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("L5HWT4Prize")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("4e7fc88a-3423-4d07-9113-89f1a7eda7dc")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
