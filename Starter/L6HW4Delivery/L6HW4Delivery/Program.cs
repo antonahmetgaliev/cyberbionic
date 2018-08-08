b45d4aeb7707ad4b4bfc183408f0c8748993d3d3
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-08 04:25:01 +0300

    add delivery programm

---
+++ Starter/L6HW4Delivery/L6HW4Delivery/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace L6HW4Delivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the number of clients - ");
+            string klstr = Console.ReadLine();
+            int kl = Convert.ToInt32(klstr), del=1;
+            do
+            {
+                del *= kl--;
+            }
+            while (kl >0);
+            Console.WriteLine("Number of routes = {0}", del);
+            Console.ReadKey();
+        }
+    }
+}
