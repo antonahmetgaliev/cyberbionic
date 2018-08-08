10cc8a4ccc1f11681ae3698dcb5f14db4d33626e
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-08 04:11:37 +0300

    add figures programm

---
+++ Starter/L6HWT3Figures/L6HWT3Figures/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace L6HWT3Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rectangle:");
+            for (int i=0;i<5;++i)
+            {
+                for (int ii=0;ii<5;++ii)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+            Console.WriteLine("Right triangle:");
+            int iii = 1;
+            for (int i = 0; i < 5; ++i)
+            {
+                for (int ii = 0; ii < iii; ++ii)
+                    Console.Write("*");
+                Console.Write("\n");
+                ++iii;
+            }
+            Console.WriteLine("Equilateral triangle:");
+            int ir = 3, il = 3;
+            for (int i = 0; i < 5; ++i)
+            {
+                for (int ii = 0; ii < il; ++ii)
+                {
+                    if (ii > ir && ii < il) Console.Write("*");
+                    else Console.Write(" ");
+                }
+                Console.Write("\n");
+                --ir;
+                ++il;
+            }
+            Console.ReadKey();
+        }
+    }
+}
