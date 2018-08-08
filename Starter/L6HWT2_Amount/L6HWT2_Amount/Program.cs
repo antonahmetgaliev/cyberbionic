531468b0979ae69f98de8e617a41469bec3c4f47
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-08 03:52:12 +0300

    add amount programm

---
+++ Starter/L6HWT2_Amount/L6HWT2_Amount/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace L6HWT2_Amount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input interval A - ");
+            string Astr = Console.ReadLine();
+            Console.Write("Input interval B - ");
+            string Bstr = Console.ReadLine();
+            int A = Convert.ToInt32(Astr), B = Convert.ToInt32(Bstr);
+            int sum = A, i = A;            
+            while (i<B)
+            {
+                if (i < 0) Console.WriteLine(i);
+                ++i;
+                if (i < 0)
+                    sum -= i;
+                else
+                    sum += i;                
+            }            
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
