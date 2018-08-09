ab711bcb370dcd95751565f74f3b295a32491799
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-09 12:58:05 +0300

    add Avarage programm

---
+++ Starter/L7HWExtra1Average/L7HWExtra1Average/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace L7HWExtra1Average
+{
+    class Program
+    {
+        static int Cv()
+        {
+            string inp = Console.ReadLine();
+            return Convert.ToInt32(inp);            
+        }
+        static double Calculate(double a, double b, double c)
+        {
+            return (a + b + c) / 3;
+        }
+
+        static void Main()
+        {
+            Console.Write("Input a - ");            
+            int a = Cv();
+            Console.Write("Input b - ");
+            int b = Cv();
+            Console.Write("Input c - ");
+            int c = Cv();
+            double rez = Calculate(a, b, c);
+            Console.WriteLine("Avarage of {1}, {2}, {3} = {0}",rez,a,b,c);
+            Console.ReadKey();
+        }
+    }
+}
