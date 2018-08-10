79dd224397fe9a03d95dba1a544e5a20e92eff34
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-10 11:49:39 +0300

    add empty L8HWT4Calculator project

---
+++ Starter/L8HWT4Calculator/L8HWT4Calculator/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace L8HWT4Calculator
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Starter/L8HWT4Calculator/L8HWT4Calculator/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace L8HWT4Calculator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
