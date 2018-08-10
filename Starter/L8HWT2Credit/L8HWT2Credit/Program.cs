ce44d351b8a2030c384543932184f03e20775c10
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-10 11:46:26 +0300

    add empty L8HWT3Delivery project

--- Starter/L8HWT2Credit/L8HWT2Credit/Program.cs
+++ Starter/L8HWT2Credit/L8HWT2Credit/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace L8HWT2Credit
+﻿using System;
+
+namespace L8HWT2Credit
 {
     class Program
     {
         static void Main()
         {
+            Console.Write("Input amount of payment - ");
+            int payment;
+            int.TryParse(Console.ReadLine(),out payment);
         }
     }
 }
---
+++ Starter/L8HWT3Delivery/L8HWT3Delivery/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace L8HWT3Delivery
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
