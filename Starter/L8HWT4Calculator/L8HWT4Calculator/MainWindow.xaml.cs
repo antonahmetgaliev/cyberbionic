3c58eab15fbf09eee6ba9a3b9b78bb2341f9c9a8
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-12 17:38:34 +0300

    add L8HWT4Calculator project

--- Starter/L8HWT4Calculator/L8HWT4Calculator/MainWindow.xaml.cs
+++ Starter/L8HWT4Calculator/L8HWT4Calculator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace L8HWT4Calculator
 {
@@ -10,6 +11,35 @@ namespace L8HWT4Calculator
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int operand1, operand2;
+            int.TryParse(TB1.Text, out operand1);
+            int.TryParse(TB2.Text, out operand2);
+            if (Mod.IsChecked == true)
+            {
+                TB3.Text = Convert.ToString( operand1 % operand2);
+            }
+            else if (Deg.IsChecked == true)
+            {
+                TB3.Text = Convert.ToString(Math.Pow(operand1,operand2));
+            }
+            else if (Con.IsChecked == true)
+            {
+                for (int i = operand2; i != 0; i /= 10)
+                {
+                    operand1 *= 10;
+                    operand1 += operand2;
+                }
+
+                TB3.Text = Convert.ToString(Math.Pow(operand1, operand2));
+            }
+            else if (Div.IsChecked == true)
+            {
+                TB3.Text = Convert.ToString(operand1/operand2);
+            }
         }
     }
 }
