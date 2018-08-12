4057455b7d3987f99ad5bc8e7d11d15b7f74962b
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-12 11:20:02 +0300

    add L8HWT2Credit project

--- Starter/L8HWT2Credit/L8HWT2Credit/Program.cs
+++ Starter/L8HWT2Credit/L8HWT2Credit/Program.cs
@@ -4,11 +4,22 @@ namespace L8HWT2Credit
 {
     class Program
     {
+        static void credit(int pay)
+        {
+            int debt = 700;
+            Console.WriteLine("The client paid {0} $ of debt",pay);
+            int debtCheck = debt - pay;
+            if (debtCheck<0) Console.WriteLine("Prepayment is - {0} $",-debtCheck);
+            else if  (debtCheck == 0) Console.WriteLine("All debt paid");
+            else Console.WriteLine("Debt is - {0} $", debtCheck);
+        }
         static void Main()
         {
             Console.Write("Input amount of payment - ");
             int payment;
             int.TryParse(Console.ReadLine(),out payment);
+            credit(payment);
+            Console.ReadKey();
         }
     }
 }
