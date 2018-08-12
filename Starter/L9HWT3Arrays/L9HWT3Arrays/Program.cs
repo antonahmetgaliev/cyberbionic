2bec58302209561bdc5bafe2dedc4085391e2e06
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-12 17:57:55 +0300

    add empty L9HWT3Arrays project

---
+++ Starter/L9HWT3Arrays/L9HWT3Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L9HWT3Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
