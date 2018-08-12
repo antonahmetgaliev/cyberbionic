7b291d55a7bb7721ca452f5696526225ccabbf14
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-13 00:52:53 +0300

    add L9HWE1Array project

--- Starter/L9HWE1Array/L9HWE1Array/Program.cs
+++ Starter/L9HWE1Array/L9HWE1Array/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace L9HWE1Array
+﻿using System;
+
+namespace L9HWE1Array
 {
     class Program
     {
         static void Main()
         {
+            int [] array = new int[10];
+            for (int i = 0; i < array.Length; ++i)
+            {
+                array[i] = i;
+            }
+            for (int i = array.Length-1; i >= 0; --i)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.ReadKey();
         }
     }
 }
