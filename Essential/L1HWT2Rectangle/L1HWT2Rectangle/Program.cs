8444b54dcc54aea12a3317e5384848fb6c6e7303
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-14 13:10:23 +0300

    add empty L1HWT2Rectangle project

---
+++ Essential/L1HWT2Rectangle/L1HWT2Rectangle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1HWT2Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
