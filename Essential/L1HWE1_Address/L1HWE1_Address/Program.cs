a2841f62b6cc0693e7376e79477d1cfdcacd7b61
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-14 12:25:01 +0300

    add L1HWE1_Address project

---
+++ Essential/L1HWE1_Address/L1HWE1_Address/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1HWE1_Address
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
