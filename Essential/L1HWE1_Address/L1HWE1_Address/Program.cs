6e8bd9d74b1629a871650d1f36c7c5ca91e95e7d
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-14 13:02:13 +0300

    add L1HWE1_Address project

--- Essential/L1HWE1_Address/L1HWE1_Address/Program.cs
+++ Essential/L1HWE1_Address/L1HWE1_Address/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace L1HWE1_Address
 {
+    class Address
+    {
+        public string Index { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Apartment { get; set; }
+
+    }
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Address home = new Address();
+            home.Index = "133";
+            home.Country = "Ukraine";
+            home.City = "Berdichev";
+            home.Street = "Chudnovskaya";
+            home.House = "102";
+            home.Apartment = "A1";
+            Console.WriteLine(home.Index);
+            Console.WriteLine(home.Country);
+            Console.WriteLine(home.City);
+            Console.WriteLine(home.Street);
+            Console.WriteLine(home.House);
+            Console.WriteLine(home.Apartment);
+            Console.ReadKey();
         }
     }
 }
