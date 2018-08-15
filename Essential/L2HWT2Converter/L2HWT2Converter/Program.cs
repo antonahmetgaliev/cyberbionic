37efdd0cfb8ebb65b85781465ce645744e171c42
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-16 00:14:44 +0300

    correct L2HWE2Converter project

--- Essential/L2HWT2Converter/L2HWT2Converter/Program.cs
+++ Essential/L2HWT2Converter/L2HWT2Converter/Program.cs
@@ -2,33 +2,6 @@
 
 namespace L2HWT2Converter
 {
-    class Converter
-    {
-        private readonly double _usd;
-        private readonly double _eur;
-        private readonly double _rub;
-
-        public Converter(double usd, double eur, double rub)
-        {
-            _usd = usd;
-            _eur = eur;
-            _rub = rub;
-        }
-
-        public double ConvertToCurr(double uah, string curr)
-        {
-            if (curr == "USD") return uah / _usd;
-            if (curr == "EUR") return uah / _eur;
-            return uah / _rub;
-        }
-
-        public double ConvertToUah(double currDoub, string currStr)
-        {
-            if (currStr == "USD") return currDoub * _usd;
-            if (currStr == "EUR") return currDoub * _eur;
-            return currDoub * _rub;
-        }
-    }
     class Program
     {
         static bool Try()
