c1ce156ae2cd18abd9e40bdbfb0d81a0b681c82f
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-15 23:08:37 +0300

    add unfinished project L2HWE2Converter

--- Essential/L2HWE1User/L2HWE1User/Data.cs
+++ Essential/L2HWE1User/L2HWE1User/Data.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace L2HWE1User
+﻿namespace L2HWE1User
 {
     class Data
     {
+        private readonly User[] _user;
+        private string DataInfo { get; }
+
+        public Data(string data)
+        {
+            _user = new User[10];
+            DataInfo = data;
+        }
+
+        public void InputUser(string login, string name, string surname, string age, int id)
+        {
+            _user[id] = new User(login, name, surname, age, DataInfo);
+        }
+
+        public void ShowUser(int id)
+        {
+            _user[id].Show();
+        }
     }
 }
--- Essential/L2HWE1User/L2HWE1User/User.cs
+++ Essential/L2HWE1User/L2HWE1User/User.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace L2HWE1User
 {
     class User
     {
+        private string Login { get; }
+        private string Name { get; }
+        private string Surname { get; }
+        private string Age { get; }
+        private string Data { get; }
+
+        public User(string login, string name, string surname, string age, string data)
+        {
+            Login = login;
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Data = data;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("\nLogin - {0}", Login);
+            Console.WriteLine("\nName of user - {0}", Name);
+            Console.WriteLine("Surname of user - {0}", Surname);
+            Console.WriteLine("Age of user - {0}", Age);
+        }
     }
 }
