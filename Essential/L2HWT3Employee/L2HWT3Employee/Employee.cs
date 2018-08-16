e9a3ed3a126c34c73f446bc8e049c186ae4e97f4
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-16 12:49:54 +0300

    add L2HWE3Employee project

--- Essential/L2HWT3Employee/L2HWT3Employee/Employee.cs
+++ Essential/L2HWT3Employee/L2HWT3Employee/Employee.cs
@@ -1,12 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace L2HWT3Employee
 {
     class Employee
     {
+        private string _name;
+        private string _surname;
+        private string _position;
+        private double _experience;
+        private const double tax = 0.18;
+        private const double salary = 1000;
+
+        public Employee(string name, string surname)
+        {
+            _name = name;
+            _surname = surname;
+        }
+
+        public double Salary(string position, double experience)
+        {
+            _experience = experience;
+            switch (_position = position)
+            {
+                case "accountant":
+                    return SalaryCalcul(salary * 1.5, _experience);
+                case "security":
+                    return SalaryCalcul(salary * 0.75, _experience);
+                default:
+                    return SalaryCalcul(salary * 2.5, _experience);
+            }
+        }
+
+        private double SalaryCalcul(double salary, double experience)
+        {
+            if (experience >= 5)
+                if (experience >= 10)
+                    if (experience >= 15)
+                        if (experience >= 20)
+                            if (experience >= 25)
+                            return salary + salary * 0.5; 
+                            else
+                            return salary + salary * 0.45;
+                        else
+                        return salary + salary * 0.35; 
+                    else
+                    return salary + salary * 0.25;  
+                else
+                return salary + salary * 0.15; 
+             return salary + salary * 0.1;
+        }
+
+        public void Show(double salary)
+        {
+            Console.WriteLine("\nName : {0} \nSurname : {1} \nPosition : {2} \nExperience : {3} \nSalary : {4} USD \nTax charge : {5} USD", _name, _surname, _position, _experience, salary, salary*tax);
+        }
     }
 }
--- Essential/L2HWT3Employee/L2HWT3Employee/Program.cs
+++ Essential/L2HWT3Employee/L2HWT3Employee/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace L2HWT3Employee
 {
     class Program
     {
-        static void Main(string[] args)
+        static bool Try()
         {
+            Console.Write("Do u want to try again? (Y/N)");
+            var check = Console.ReadKey();
+            Console.Clear();
+            return check.Key == ConsoleKey.Y;
+        }
+        static void Main()
+        {
+            do
+            {
+                Console.Write("Input name od worker - ");
+                var name = Console.ReadLine();
+                Console.Write("Input surname od worker - ");
+                var surname = Console.ReadLine();
+                var posCheck = false;
+                string position;
+                do
+                {
+                    Console.Write("Input position od worker (accountant,security,director)- ");
+                    position = Console.ReadLine();
+                    if (position != "accountant" && position != "security" && position != "director")
+                        Console.WriteLine("Error of position entry!");
+                    else posCheck = true;
+                } while (!posCheck);
+
+                Console.Write("Input experience of worker - ");
+                var experience = double.Parse(Console.ReadLine());
+                var employee = new Employee(name, surname);
+                var salary = employee.Salary(position, experience);
+                employee.Show(salary);
+            } while (Try());
         }
     }
 }
