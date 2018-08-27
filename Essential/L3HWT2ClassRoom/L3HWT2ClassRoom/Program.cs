68c8604af6dec129a3b0b6a8b94c28755c433f60
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-08-27 14:20:43 +0300

    add L3HWT2ClassRoom empty project

---
+++ Essential/L3HWT2ClassRoom/L3HWT2ClassRoom/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace L3HWT2ClassRoom
+{
+    class Program
+    {
+        class ClassRoom
+        {
+
+        }
+
+        class Pupil
+        {
+            void Study()
+            {
+
+            }
+
+            void Read()
+            {
+
+            }
+
+            void Write()
+            {
+
+            }
+
+            void Relax()
+            {
+
+            }
+
+        }
+
+        class ExcelentPupil:Pupil
+        {
+            
+        }
+
+        class GoodPupil:Pupil
+        {
+            
+        }
+
+        class BadPupil:Pupil
+        {
+            
+        }
+
+        static void Main()
+        {
+        }
+    }
+}
