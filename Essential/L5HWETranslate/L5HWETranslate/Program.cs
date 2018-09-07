655ec971cf510813fd7cc4c75e10ebe401ce95d5
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-09-07 18:06:17 +0300

    add L5HWETranslate project

---
+++ Essential/L5HWETranslate/L5HWETranslate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace L5HWETranslate
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var dictionary = new Dictionary();
+            do
+            {
+                Console.Write("Enter word - ");
+                var word = Console.ReadLine();
+                Console.WriteLine(dictionary[word]);
+            } while (Try());
+
+            Console.WriteLine();
+            Console.WriteLine(new string('-', 20));
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine(dictionary[i]);
+            }
+            
+            Console.ReadKey();
+        }
+
+        private static bool Try()
+        {
+            Console.Write("\nDo u want to enter other word? (Y/N)");
+            var exit = Console.ReadKey();
+            return exit.Key != ConsoleKey.N;
+        }
+    }
+}
