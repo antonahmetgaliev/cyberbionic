81276efb3afc55bc9437c48cff465d57a71abc71
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2018-09-11 19:17:44 +0300

    add L5HWT2Array project

---
+++ Essential/L5HWT2Array/L5HWT2Array/NumArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace L5HWT2Array
+{
+    internal class NumArray
+    {
+        private readonly int[] _key;
+
+        public NumArray(int arrayDimension)
+        {
+            _key = new int[arrayDimension];
+        }
+
+        public void EnterElements()
+        {
+            var rand = new Random();
+            for (var i = 0; i < _key.Length; i++)
+            {
+                _key[i] = rand.Next(-100, 100);
+            }
+        }
+
+        public int MaxArr()
+        {
+            return _key.Max();
+        }
+
+        public int MinArr()
+        {
+            return _key.Min();
+        }
+
+        public int SummArr()
+        {
+            return _key.Sum();
+        }
+
+        public int this[int index]
+        {
+            get => _key[index];
+            set => _key[index] = value;
+        }
+    }
+}
--- Essential/L5HWT2Array/L5HWT2Array/Program.cs
+++ Essential/L5HWT2Array/L5HWT2Array/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace L5HWT2Array
 {
-    class Program
+    internal class Program
     {
-        static void Main()
+        private static int CheckInt()
         {
+            int value;
+            var text = Console.ReadLine();
+            while (!int.TryParse(text, out value))
+            {
+                Console.Write("\nError Input! Please, enter other value - ");
+                text = Console.ReadLine();
+            }
+
+            return value;
+        }
+        private static void Main()
+        {
+            Console.Write("Input array of dimension - ");
+            var n = CheckInt();
+            var array = new NumArray(n);
+            array.EnterElements();
+            for (var i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine("\nMax element - {0}", array.MaxArr());
+            Console.WriteLine("Min element - {0}", array.MinArr());
+            Console.WriteLine("Summ - {0}", array.SummArr());
+            Console.WriteLine("Average - {0}", array.SummArr()/n);
+            Console.WriteLine("All negative elements :");
+            for (var i = 0; i < n; i++)
+            {
+                if (array[i] < 0) Console.Write("{0} ", array[i]);
+            }
+
+            Console.ReadKey();
         }
     }
 }
