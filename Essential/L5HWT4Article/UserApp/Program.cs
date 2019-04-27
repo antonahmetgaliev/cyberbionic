1ec28f9454153b0d570e0b3d5015db2e277f7bb9
Anton Ahmetgaliev <antonahmetgaliev4@gmail.com>
2019-04-27 21:45:24 +0300

    first commit

---
+++ Essential/L5HWT4Article/UserApp/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace UserApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Essential/L5HWT4Article/UserApp/User.cs
@@ -0,0 +1,6 @@
+﻿namespace UserApp
+{
+    internal class User
+    {
+    }
+}
