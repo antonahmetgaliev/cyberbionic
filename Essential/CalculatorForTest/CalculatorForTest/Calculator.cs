24022bde2ad9ef7101efc1bc1a51415e1346f3fe
Anton Ahmetgaliev <antonahmetgaliev4@gmail.com>
2019-04-29 14:23:14 +0300

    add tests metod for Sub, Mul, Div

--- Essential/CalculatorForTest/CalculatorForTest/Calculator.cs
+++ Essential/CalculatorForTest/CalculatorForTest/Calculator.cs
@@ -6,5 +6,20 @@
         {
             return a + b;
         }
+
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Mul(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Div(double a, double b)
+        {
+            return a / b;
+        }
     }
 }
--- Essential/CalculatorForTest/CalculatorTest/CalculatorTests.cs
+++ Essential/CalculatorForTest/CalculatorTest/CalculatorTests.cs
@@ -21,5 +21,26 @@ namespace CalculatorTest
             var actual = _target.Sum(1.5, 2.3);
             Assert.AreEqual(3.8,actual);
         }
+
+        [TestMethod]
+        public void SubTest()
+        {
+            var actual = _target.Sub(1.5, 2.3);
+            Assert.AreEqual(-0.8,actual);
+        }
+
+        [TestMethod]
+        public void MulTest()
+        {
+            var actual = _target.Mul(2.5, 2);
+            Assert.AreEqual(5,actual);
+        }
+
+        [TestMethod]
+        public void DivTest()
+        {
+            var actual = _target.Div(2.8, 1.4);
+            Assert.AreEqual(2,actual);
+        }
     }
 }
