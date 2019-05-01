915870a0c678a8edf29a48888d10a4be3e13dff0
Anton Ahmetgaliev <antonahmetgaliev4@gmail.com>
2019-05-01 16:21:03 +0300

    add ConsoleExtension.cs

--- MyLib/Common/Common/ConsoleExtension.cs
+++ MyLib/Common/Common/ConsoleExtension.cs
@@ -1,6 +1,38 @@
-﻿namespace Common
+﻿using System;
+
+namespace Common
 {
-    public partial class Class1
+    public static class ConsoleExtension
     {
+        public static int CheckInt()
+        {
+            int value;
+            var text = Console.ReadLine();
+            while (!int.TryParse(text, out value))
+            {
+                Console.Write("\nError Input! Please, enter other value - ");
+                text = Console.ReadLine();
+            }
+            return value;
+        }
+
+        public static double CheckDouble()
+        {
+            double value;
+            var text = Console.ReadLine();
+            while (!double.TryParse(text, out value))
+            {
+                Console.Write("\nError Input! Please, enter other value - ");
+                text = Console.ReadLine();
+            }
+
+            return value;
+        }
+
+        public static void WaitingAction()
+        {
+            Console.WriteLine("\nPress any button to continue.");
+            Console.ReadKey();
+        }
     }
 }
