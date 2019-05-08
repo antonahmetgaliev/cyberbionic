1289694f4f1cdaa3ce0033bd57c67f9986c726cc
Anton Ahmetgaliev <antonahmetgaliev4@gmail.com>
2019-05-08 12:22:27 +0300

    added birthday input from the keyboard.

--- Essential/L8HWEBirthday/L8HWEBirthday/Program.cs
+++ Essential/L8HWEBirthday/L8HWEBirthday/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace L8HWEBirthday
+﻿using Common;
+using System;
+
+namespace L8HWEBirthday
 {
     internal class Program
     {
         private static void Main()
         {
+            Console.Write("Enter the year of your birth - ");
+            var birthYear = ConsoleExtension.CheckInt();
+            Console.Write("Enter the month of your birth - ");
+            var birthMonth = ConsoleExtension.CheckInt();
+            Console.Write("Enter the day of your birth - ");
+            var birthDay = ConsoleExtension.CheckInt();
         }
     }
 }
