ceaaaa60ed443f1813d6c4207e201b5d7224e014
Anton Ahmetgaliev <antonahmetgaliev4@gmail.com>
2019-05-08 12:32:16 +0300

    added time left initialize

--- Essential/L8HWEBirthday/L8HWEBirthday/Program.cs
+++ Essential/L8HWEBirthday/L8HWEBirthday/Program.cs
@@ -7,14 +7,17 @@ namespace L8HWEBirthday
     {
         private static void Main()
         {
-            Console.Write("Enter the year of your birth - ");
-            var birthYear = ConsoleExtension.CheckInt();
             Console.Write("Enter the month of your birth - ");
             var birthMonth = ConsoleExtension.CheckInt();
             Console.Write("Enter the day of your birth - ");
             var birthDay = ConsoleExtension.CheckInt();
-            var birthday = new DateTime(birthYear, birthMonth, birthDay);
             var today = DateTime.Now;
+            int leftYear;
+            if (today.Month > birthMonth) leftYear = today.Year;
+            else leftYear = today.Year + 1;
+            var birthday = new DateTime(leftYear, birthMonth, birthDay);
+            var left = birthday - today;
+
         }
     }
 }
