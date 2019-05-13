1e569d7aeedeabbee5e16304826afb9192382d0b
Anton Ahmetgaliev <antonahmetgaliev4@gmail.com>
2019-05-13 16:54:01 +0300

    created delegate, anonymous metod

--- Essential/L9HWEAnonymous/L9HWEAnonymous/Program.cs
+++ Essential/L9HWEAnonymous/L9HWEAnonymous/Program.cs
@@ -2,8 +2,12 @@
 {
     internal class Program
     {
+        public delegate double AverageDelegate(int a, int b, int c);
+
         private static void Main()
         {
+            AverageDelegate averageDelegate = (a, b, c) => (a + b + c) / 3;
+            //var average = averageDelegate(2, 2, 2);
         }
     }
 }
