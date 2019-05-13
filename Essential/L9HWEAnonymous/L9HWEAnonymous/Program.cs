c6d0789a57577cec4e2c17751622abf386e3b1c1
Anton Ahmetgaliev <antonahmetgaliev4@gmail.com>
2019-05-13 17:00:54 +0300

    finished L9HWEAnonymous project

--- Essential/L9HWEAnonymous/L9HWEAnonymous/Program.cs
+++ Essential/L9HWEAnonymous/L9HWEAnonymous/Program.cs
@@ -1,13 +1,22 @@
-﻿namespace L9HWEAnonymous
+﻿using System;
+using Common;
+
+namespace L9HWEAnonymous
 {
     internal class Program
     {
-        public delegate double AverageDelegate(int a, int b, int c);
+        public delegate double AverageDelegate(double a, double b, double c);
 
         private static void Main()
         {
+            Console.WriteLine("Enter 3 numbers:");
+            var num1 = ConsoleExtension.CheckInt();
+            var num2 = ConsoleExtension.CheckInt();
+            var num3 = ConsoleExtension.CheckInt();
             AverageDelegate averageDelegate = (a, b, c) => (a + b + c) / 3;
-            //var average = averageDelegate(2, 2, 2);
+            var average = averageDelegate(num1, num2, num3);
+            Console.WriteLine($"\nAverage of {num1}, {num2}, {num3} = {average}");
+            Console.ReadKey();
         }
     }
 }
