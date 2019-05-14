b35f144fe1e01d60b176e94adc86c4108d87a25b
Anton Ahmetgaliev <antonahmetgaliev4@gmail.com>
2019-05-14 17:43:40 +0300

    finished Calculator.cs, CalculatorTests,cs

--- Essential/L9HWT2DelegateCalculator/L9HWT2DelegateCalculator/Calculator.cs
+++ Essential/L9HWT2DelegateCalculator/L9HWT2DelegateCalculator/Calculator.cs
@@ -1,7 +1,38 @@
-﻿namespace L9HWT2DelegateCalculator
+﻿using System;
+
+namespace L9HWT2DelegateCalculator
 {
-    public class Calculator
+    public static class Calculator
     {
-        
+        private delegate int DelegateCalculator(int a, int b);
+
+        public static int Sum(int a, int b)
+        {
+            DelegateCalculator delegateSum = (i, i1) => a + b;
+            return delegateSum(a, b);
+        }
+        public static int Sub(int a, int b)
+        {
+            DelegateCalculator delegateSub = (i, i1) => a - b;
+            return delegateSub(a, b);
+        }
+        public static int Mul(int a, int b)
+        {
+            DelegateCalculator delegateMul = (i, i1) => a * b;
+            return delegateMul(a, b);
+        }
+        public static int Div(int a, int b)
+        {
+            DelegateCalculator delegateDiv = (i, i1) => a / b;
+            try
+            {
+                return delegateDiv(a, b);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division by 0 is prohibited.");
+                throw;
+            }
+        }
     }
 }
--- Essential/L9HWT2DelegateCalculator/L9HWT2DelegateCalculatorTests/CalculatorTests.cs
+++ Essential/L9HWT2DelegateCalculator/L9HWT2DelegateCalculatorTests/CalculatorTests.cs
@@ -1,14 +1,46 @@
 ﻿using System;
+using L9HWT2DelegateCalculator;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace L9HWT2DelegateCalculatorTests
 {
     [TestClass]
-    public class UnitTest1
+    public class CalculatorTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SumTest()
         {
+            var actual = Calculator.Sum(1, 2);
+            Assert.AreEqual(3, actual);
+        }
+
+        [TestMethod]
+        public void SubTest()
+        {
+            var actual = Calculator.Sub(2, 1);
+            Assert.AreEqual(1, actual);
+        }
+
+        [TestMethod]
+        public void MulTest()
+        {
+            var actual = Calculator.Mul(3, 2);
+            Assert.AreEqual(6, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DivByZeroExceptionTest()
+        {
+            var actual = Calculator.Div(2, 0);
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod]
+        public void DivTest()
+        {
+            var actual = Calculator.Div(4, 2);
+            Assert.AreEqual(2, actual);
         }
     }
 }
