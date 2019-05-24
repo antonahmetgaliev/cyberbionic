2f37a5804441f1e20e55afc18bfe808299872196
Anton Ahmetgaliev <antonahmetgaliev4@gmail.com>
2019-05-24 12:13:38 +0300

    correct L1HWT2Rectangle

--- Essential/L1HWT2Rectangle/L1HWT2Rectangle/Program.cs
+++ Essential/L1HWT2Rectangle/L1HWT2Rectangle/Program.cs
@@ -2,45 +2,47 @@
 
 namespace L1HWT2Rectangle
 {
-    class Rectangle
+    internal class Rectangle
     {
-        private double side1 = 0;
-        private double side2 = 0;
+        private readonly double _side1;
+        private readonly double _side2;
 
         public Rectangle(double side1, double side2)
         {
-            this.side1 = side1;
-            this.side2 = side2;
+            _side1 = side1;
+            _side2 = side2;
         }
 
         public double AreaCalculator()
         {
-            return side1 * side2;
+            return _side1 * _side2;
         }
 
         public double PerimeterCalculator()
         {
-            return 2 * side1 + 2 * side2;
+            return 2 * _side1 + 2 * _side2;
         }
 
         public double Area => AreaCalculator();
         public double Perimetr => PerimeterCalculator();
     }
-    class Program
+
+    internal class Program
     {
-        static double Read(string numbstr)
+        private static double Read(string numbstr)
         {
-            bool result = int.TryParse(numbstr, out var number);
+            var result = int.TryParse(numbstr, out var number);
             Console.WriteLine(result == true ? "Conversion was successful" : "Conversion failed");
             return number;
         }
-        static void Main()
+
+        private static void Main()
         {
             Console.Write("Input side 1 - ");
-            double side1 = Read(Console.ReadLine());
+            var side1 = Read(Console.ReadLine());
             Console.Write("Input side 2 - ");
-            double side2 = Read(Console.ReadLine());
-            Rectangle rectangle = new Rectangle(side1, side2);
+            var side2 = Read(Console.ReadLine());
+            var rectangle = new Rectangle(side1, side2);
             Console.WriteLine("Area = {0}",rectangle.Area);
             Console.WriteLine("Perimetr = {0}",rectangle.Perimetr);
             Console.ReadKey();
