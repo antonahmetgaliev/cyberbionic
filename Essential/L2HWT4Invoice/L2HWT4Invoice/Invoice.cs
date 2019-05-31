513393c1e975f00435dc5cfa367a8d5fec477e51
Anton Ahmetgaliev <antonahmetgaliev4@gmail.com>
2019-06-01 00:01:21 +0300

    corrected some mistakes

--- Essential/L2HWT4Invoice/L2HWT4Invoice/Invoice.cs
+++ Essential/L2HWT4Invoice/L2HWT4Invoice/Invoice.cs
@@ -1,12 +1,20 @@
 ﻿namespace L2HWT4Invoice
 {
-    class Invoice
+    internal class Invoice
     {
-        private int account;
-        private string customer;
-        private string provider;
-        private string article;
-        private int quantity;
+        private int _account;
+        private string _customer;
+        private string _provider;
+        private string _article;
+        private int _quantity;
 
+        public Invoice(int account, string customer, string provider, int quantity, string article)
+        {
+            this._account = account;
+            this._customer = customer;
+            this._provider = provider;
+            this._quantity = quantity;
+            this._article = article;
+        }
     }
 }
