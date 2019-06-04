588943f45e38dfded0a3a4e6c6d7d256bb379507
Anton Ahmetgaliev <antonahmetgaliev4@gmail.com>
2019-06-04 21:36:13 +0300

    added not finished project

---
+++ Essential/L10HWEGenerics/L10HWEGenerics/MyClass.cs
@@ -0,0 +1,11 @@
+﻿namespace L10HWEGenerics
+{
+    internal class MyClass<T>
+    {
+        public T FactoryMethod()
+        {
+            T field = default;
+            return field;
+        }
+    }
+}
