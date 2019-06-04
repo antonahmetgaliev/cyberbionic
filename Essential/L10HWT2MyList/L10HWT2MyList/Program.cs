96c75c2d0e4765b7c2dd111452d01a52e775457f
Anton Ahmetgaliev <antonahmetgaliev4@gmail.com>
2019-06-04 21:43:50 +0300

    added L10HWT2MyList project

---
+++ Essential/L10HWT2MyList/L10HWT2MyList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace L10HWT2MyList
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var a = new List<int>();
+        }
+    }
+}
